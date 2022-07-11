6e29d4907e6774249f68c12e7d426f0105cf9e6e
Callank21 <95372216+Callank21@users.noreply.github.com>
2022-07-11 16:05:16 -0700

    finished footer links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,15 @@
+import twitter from '../../assets/twittericon.svg';
+import upwork from '../../assets/upworkicon.svg';
+import github from '../../assets/giticon.svg';
+
+
 function Footer() {
     return (
     <footer>
-        <div>
-        <h1> Callan Keesling</h1>
+        <div className="footerContainer">
+        <a href="https://twitter.com/KeeslingCallan" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="twitter link"/></a>
+        <a href="https://www.upwork.com/freelancers/~0152dd60ce74eaf031" target="_blank" rel="noopener noreferrer"><img src={upwork} alt="upwork link"/></a>
+        <a href="https://github.com/Callank21" target="_blank" rel="noopener noreferrer"><img src={github} alt="github link"/></a>
         </div>
     </footer>
     )
