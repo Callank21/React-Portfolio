235565c08c9e14dc0943264f6fc7442b310b63d4
Callank21 <95372216+Callank21@users.noreply.github.com>
2022-07-11 15:34:50 -0700

    finished resume tab

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,6 +1,30 @@
+import resume from '../../assets/Profile.pdf';
+
 function Resume() {
     return (
-        <div>Resume</div>
+        <div className="resumeForm">
+            <p className="subHead">Resume</p>
+            <p> Download resume <a href={resume} download>here</a> </p>
+            <p className="paragraph"> Front-End Proficiencies</p>
+            <ul className="list">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>responsive design</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+            </ul>
+            <p className="paragraph"> Back-End Proficiencies</p>
+            <ul className="list">
+                <li>APIs</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>MySQL, Sequelize</li>
+                <li>MongoDB, Mongoose</li>
+                <li>REST</li>
+                <li>GraphQL</li>
+            </ul>
+        </div>
     )
 }
 
